5b870a6dd83cff1223b4ab4c85366cf00cc4ca02
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-03-12 22:34:46 +0200

    updates i think

---
+++ L1T04 - JavaScript Programming, Variables and Data Types/fortuneTeller.js
@@ -0,0 +1,6 @@
+let mothersName = prompt("Enter your Mothers name: ");
+let streetName = prompt("Enter the name of the Street you grew up on: ");
+let favouriteColour = prompt("Enter your favourite colour as a child: ");
+let currentAge = prompt("Enter your current age: ");
+let inoutNumber = prompt("Enter a number between 1 and 10: ");
+

4a410f6ce9ca195829fb68770c763f4387caf8c4
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-03-12 23:11:32 +0200

    updates

--- L1T04 - JavaScript Programming, Variables and Data Types/fortuneTeller.js
+++ L1T04 - JavaScript Programming, Variables and Data Types/fortuneTeller.js
@@ -2,5 +2,17 @@ let mothersName = prompt("Enter your Mothers name: ");
 let streetName = prompt("Enter the name of the Street you grew up on: ");
 let favouriteColour = prompt("Enter your favourite colour as a child: ");
 let currentAge = prompt("Enter your current age: ");
-let inoutNumber = prompt("Enter a number between 1 and 10: ");
+let numberBetween = prompt("Enter a number between 1 and 10: ");
 
+let yearsToMeet = numberBetween;
+let bestFriendsName = `${mothersName} ${streetName}`;
+let yearsToMarriage = Number(currentAge) + Number(numberBetween);
+let expectedChildren = Number(currentAge) % Number(numberBetween);
+let yearsToDyeHair = Math.round(Number(currentAge) / Number(numberBetween));
+let dyeColour = favouriteColour;
+
+// Template literals:
+console.log(`In ${yearsToMeet} years you are going to meet your best friend ${bestFriendsName}.
+You will get married in ${yearsToMarriage} years and have ${expectedChildren} children.
+In ${yearsToDyeHair} years you are going to dye your hair ${dyeColour}.
+`);

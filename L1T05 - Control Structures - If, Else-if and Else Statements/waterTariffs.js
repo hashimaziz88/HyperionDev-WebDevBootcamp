a8593cf07f5a931c46069321d31fcdbac667db74
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-03-17 14:43:56 +0200

    updates

--- L1T05 - Control Structures - If, Else-if and Else Statements/waterTariffs.js
+++ L1T05 - Control Structures - If, Else-if and Else Statements/waterTariffs.js
@@ -3,25 +3,25 @@ function calculateBill() {
 
   let tariff = 0;
 
-  // Calculate tariff based on water usage
+  // Calculating the tariff in Rands based on water usage
   if (waterUsage <= 6000) {
-    tariff = waterUsage * (15.73 / 1000); // R15.73 per kilolitre
+    tariff = waterUsage * (15.73 / 1000); // Tariff atR15.73 per kilolitre
   } else if (waterUsage <= 10500) {
     tariff = 6000 * (15.73 / 1000) + (waterUsage - 6000) * (22.38 / 1000); // R15.73 per kilolitre for the first 6000 litres, R22.38 per kilolitre for the rest
   } else if (waterUsage <= 35000) {
     tariff =
       6000 * (15.73 / 1000) +
       4500 * (22.38 / 1000) +
-      (waterUsage - 10500) * (31.77 / 1000); // R15.73 per kilolitre for the first 6000 litres, R22.38 per kilolitre for the next 4500 litres, R31.77 per kilolitre for the rest up to 35000 litres
+      (waterUsage - 10500) * (31.77 / 1000); // Tariff at R15.73 per kilolitre for the first 6000 litres, R22.38 per kilolitre for the next 4500 litres, R31.77 per kilolitre for the rest up to 35000 litres
   } else {
     tariff =
       6000 * (15.73 / 1000) +
       4500 * (22.38 / 1000) +
       (35000 - 10500) * (31.77 / 1000) +
-      (waterUsage - 35000) * (69.76 / 1000); // R15.73 per kilolitre for the first 6000 litres, R22.38 per kilolitre for the next 4500 litres, R31.77 per kilolitre for the next 34000 litres, R69.76 per kilolitre for the rest
+      (waterUsage - 35000) * (69.76 / 1000); // Tariff at R15.73 per kilolitre for the first 6000 litres, R22.38 per kilolitre for the next 4500 litres, R31.77 per kilolitre for the next 34000 litres, R69.76 per kilolitre for the rest
   }
 
-  // Display the result
+  // Displaying the result
   document.getElementById(
     "result"
   ).textContent = `Your water bill is R${tariff.toFixed(2)}`;

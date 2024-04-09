8cdcac5ff2ba0411d9ac0e7759ee3e9bc24e039f
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-04-09 21:07:56 +0200

    changes

---
+++ L2T01 - NodeJS/my_first_module/practice.js
@@ -0,0 +1,3 @@
+const lodash = require("lodash");
+myList = ["This", "is", "a", "list", "in", "Javascript"];
+console.log(lodash.first(myList));
---
+++ L2T01 - NodeJS/my_first_task/remove_duplicates.js
@@ -0,0 +1,3 @@
+const lodash = require("lodash");
+myArray = [1, 2, 10, 100, 10, 2, 5, 6, 10, 1000, 7, 2, 100, 1, 5, 7, 10];
+console.log(lodash.uniq(myArray));

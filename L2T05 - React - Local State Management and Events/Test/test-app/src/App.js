a5e0c66d399fa362442db6a53f6da2e1ebc244bc
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-04-21 16:33:47 +0200

    push

--- L2T05 - React - Local State Management and Events/Test/test-app/src/App.js
+++ L2T05 - React - Local State Management and Events/Test/test-app/src/App.js
@@ -1,16 +1,7 @@
-// Importing components and CSS files
-import Welcome from "../src/components/Welcome.js";
-import "./App.css";
-import NameInput from "../src/components/NameInput.js";
-import Count from "./components/Count.js";
-import DisplayName from "./components/DisplayName.js";
+import BankingApp from "./components/BankingApp.js";
 
 // Create the App component
 function App() {
-  return (
-    <div className="App">
-      <DisplayName />
-    </div>
-  );
+  return <BankingApp />;
 }
 export default App;
